a2600efd1e19ddfad5c21ffbdad67718bac88f59
Rajnish <virpaterajnish@gmail.com>
2020-10-20 13:31:45 +0530

    Assignment 2

---
+++ calcapp/Calc.js
@@ -0,0 +1,5 @@
+
+module.exports.add=function(a,b){return a+b};
+module.exports.sub=function(a,b){return abs(a-b)};
+module.exports.mul=function(a,b){return a*b};
+module.exports.div=function(a,b){return a/b};
---
+++ calcapp/index.js
@@ -0,0 +1,4 @@
+const app=require('./Calc');
+
+let result=app.add(1,2);
+console.log(result);
